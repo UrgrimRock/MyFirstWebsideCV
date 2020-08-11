c5fbed674d8b2179836d28341cb1ab659da4ec84
Robert Majerski <robert.maj91@gmail.com>
2020-08-11 09:39:56 +0200

    My first Commit

---
+++ mySide/js/index.js
@@ -0,0 +1,15 @@
+let names = ['Bratek', 'Anna'];
+let person ={
+    name: 'Kamil',
+    age: 34,
+}
+let person2 ={
+    name: 'Kamil',
+    age: 34,
+}
+
+for (let name of names){
+    console.log(name);
+}
+console.log(names);
+console.log(person);
